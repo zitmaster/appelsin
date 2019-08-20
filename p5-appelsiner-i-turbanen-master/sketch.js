d777fbc17a53f62030ee5337298157e2e5a2c836
caspersiig <capsersiig@gmail.com>
2019-08-20 09:48:49 +0200

    sat billede på din røv :D
    ja

--- p5-appelsiner-i-turbanen-master/sketch.js
+++ p5-appelsiner-i-turbanen-master/sketch.js
@@ -3,6 +3,10 @@ Først laver vi et nogle variable til at lave en appelsin
  - en kugle som vi vil skyde afsted og fange i en turban
 */
 
+var turbanhoved;
+function preload() {
+  turbanhoved = loadImage('download (2).jpg');
+}
 
 // Turbanen og appelsinen
 var turban;
