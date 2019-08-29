95fda1a88a71781f821834bb5f3989b16be2dd40
zitmaster <Victorcebula@yahoo.dk>
2019-08-29 18:19:58 +0200

    jeg har flyttet det ud af mappen

---
+++ appelsin.js
@@ -0,0 +1,65 @@
+function Appelsin() {
+
+    this.x = 0;
+    this.y = 550;
+    this.rad = 20;
+    this.xspeed = 4;
+    this.yspeed = -10;
+    this.newspeed;
+    this.grav = 0.1;
+    this.col = [200, 100, 0];
+
+    this.newspeed = this.yspeed;
+    this.x = this.rad;
+
+
+    this.tegn = function () {
+
+        fill(this.col);
+        ellipse(this.x, this.y, this.rad * 2, this.rad * 2);
+    }
+
+    this.move = function () {
+        this.x += this.xspeed;
+        this.y += this.yspeed;
+        this.yspeed += this.grav;
+
+        if (this.y < 0) {
+            this.grav = 0.5;
+        } else {
+            this.grav = .1;
+        }
+
+    }
+    this.delete = function () {
+
+        if (this.x > width || this.y > height) {
+            liv -= 1;
+            if (multiplayer == false) {
+                this.newshoot();
+            } else {
+                return true;
+            }
+
+        }
+
+        if (this.x > mouseX - 40 && this.x < mouseX + 40 && this.y > mouseY - 40 && this.y < mouseY - 30 && this.yspeed >= 0) {
+            if (multiplayer == false) {
+                this.newshoot();
+            } else {
+                score += 1;
+                return true;
+            }
+        }
+
+    }
+
+    this.newshoot = function () {
+        this.x = this.rad;
+        this.y = random(400, 550);
+        this.yspeed = this.newspeed;
+        this.xspeed = random(1.5, 5);
+
+    }
+
+}
--- p5-appelsiner-i-turbanen-master/README.md
+++
@@ -1,2 +0,0 @@
-# p5-appelsiner-i-turbanen
-Brush-up forløb af p5- og programmeringskompetencer ved starten af et nyt år med Digital Teknik
